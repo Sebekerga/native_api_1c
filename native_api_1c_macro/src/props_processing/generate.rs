85975dc11cd2cc14aec2ac30a7bb7697e0686099
Kozlov Maxim <sebekerka@gmail.com>
2023-11-06 09:15:31 +0600

    clean up line length for quotes

--- native_api_1c_macro/src/props_processing/generate.rs
+++ native_api_1c_macro/src/props_processing/generate.rs
@@ -9,25 +9,47 @@ pub fn param_ty_to_ffi_set(
 ) -> TokenStream {
     match param_type {
         PropType::Bool => {
-            quote! { native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::Bool(inner_value) => self.#param_path = inner_value.clone(), }
+            quote! {
+                native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::Bool(inner_value) => {
+                    self.#param_path = inner_value.clone(),
+                }
+            }
         }
         PropType::I32 => {
-            quote! { native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::I32(inner_value) => self.#param_path = inner_value.clone(), }
+            quote! {
+                native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::I32(inner_value) => {
+                    self.#param_path = inner_value.clone()
+                },
+            }
         }
         PropType::F64 => {
-            quote! { native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::F64(inner_value) => self.#param_path = inner_value.clone(), }
+            quote! {
+                native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::F64(inner_value) => {
+                    self.#param_path = inner_value.clone()
+                },
+            }
         }
         PropType::String => {
             quote! {
-                native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::Str(inner_value) => self.#param_path
-                    = native_api_1c::native_api_1c_core::ffi::string_utils::from_os_string(inner_value).into(),
+                native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::Str(inner_value) => {
+                    self.#param_path
+                        = native_api_1c::native_api_1c_core::ffi::string_utils::from_os_string(inner_value).into()
+                },
             }
         }
         PropType::Date => {
-            quote! { native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::Date(inner_value) => self.#param_path = inner_value.clone(), }
+            quote! {
+                native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::Date(inner_value) => {
+                    self.#param_path = inner_value.clone()
+                },
+            }
         }
         PropType::Blob => {
-            quote! { native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::Blob(inner_value) => self.#param_path = inner_value.clone(), }
+            quote! {
+                native_api_1c::native_api_1c_core::ffi::provided_types::ParamValue::Blob(inner_value) => {
+                    self.#param_path = inner_value.clone()
+                },
+            }
         }
     }
 }
